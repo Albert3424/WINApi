15391d96a51fccbd5fee7e4f63734e8dfbe54762
Hanzo Hasashi <Izukudeku09@outlook.com>
2024-11-19 21:55:59 +0500

    NUM PAD DONE

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -249,6 +249,9 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		if (wParam >= '0' && wParam <= '9')
 			SendMessage(hwnd, WM_COMMAND, wParam - '0' + IDC_BUTTON_0, 0);
+		if (wParam >= 0x60 && wParam <= 0x69)
+			SendMessage(hwnd, WM_COMMAND, wParam - 0x60 + IDC_BUTTON_0, 0);
+
 		switch (wParam)
 		{
 		case VK_OEM_PERIOD: SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_POINT), 0);
@@ -262,6 +265,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 	}
 	break;
+	///////////////////////////
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
